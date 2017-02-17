905026d6e24e9e863cf85af1d3b2f37a37a3cde9
Marcos Gómez <margobra8@users.noreply.github.com>
2017-02-17 22:36:14 +0100

    Delete process.php

--- process.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $message = '';
-  foreach($_POST as $key => $value) {
-    $message .= "$key => $value\n";
-  }
-  mail('margobra8@gmail.com','New User Registration',$message);
-  header('Location: success.html');
-?>
