c089f7b27a677e75194921d3f275a6d3d06d7a36
Marcos Gómez <margobra8@gmail.com>
2017-02-17 22:44:37 +0100

    improved alerts

--- js/validation.js
+++ js/validation.js
@@ -1,9 +1,20 @@
 function validateForm() {
-    var x = document.forms["attend"]["email"].value;
-    var y = document.forms["attend"]["name"].value;
-    var z = x.search("sek.es");
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    var email = document.forms["attend"]["email"].value;
+    var valid = re.test(email)
+    var name = document.forms["attend"]["name"].value;
+    var z = email.search("sek.es");
     if (z == -1) {
-        document.getElementById("form-error").className = "form-error";
+        document.getElementById("form-error-not-sek").className = "form-error animated fadeIn";
         return false;
+    }
+    else if (valid == false || name == "") {
+        document.getElementById("form-error-not-email").className = "form-error animated fadeIn";
+        return false;
+    }
+    else {
+        document.getElementById("form-error-not-email").className = "form-error-hidden";
+        document.getElementById("form-error-not-sek").className = "form-error-hidden";
+        return true;
     }
 }
