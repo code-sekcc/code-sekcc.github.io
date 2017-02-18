0241ecbac1cd472d8e6701d2ae85fe21cb184c8c
Marcos Gómez <margobra8@users.noreply.github.com>
2017-02-18 17:33:59 +0100

    change verification order

--- js/validation.js
+++ js/validation.js
@@ -4,12 +4,13 @@ function validateForm() {
     var valid = re.test(email)
     var name = document.forms["attend"]["name"].value;
     var z = email.search("sek.es");
-    if (z == -1) {
-        document.getElementById("form-error-not-sek").className = "form-error animated fadeIn";
+    
+    if (valid == false || name == "") {
+        document.getElementById("form-error-not-email").className = "form-error animated fadeIn";
         return false;
     }
-    else if (valid == false || name == "") {
-        document.getElementById("form-error-not-email").className = "form-error animated fadeIn";
+    else if (z == -1) {
+        document.getElementById("form-error-not-sek").className = "form-error animated fadeIn";
         return false;
     }
     else {
